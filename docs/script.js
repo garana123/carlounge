d36128123ce642cdf19afaf21ed26f147319f42d
garana123 <jasi.lindern@gmail.com>
2025-05-06 21:33:18 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -288,7 +288,7 @@ document.addEventListener('DOMContentLoaded', () => {
             'tokio manji ug': 'Primärfarbe: Rasengrün <br> Sekundär: / <br> Perleffekt: Limonengrün <br> Reifenqualm: ./. <br> Unterboden: ./.',
             'fib dn 60 vip2': 'Info: Bekommt 40% auf alle Fahrzeuge',
             'familie makarov vip2': 'Info: Bekommen 40% auf alle Fahrzeuge',
-            'fib dn 78 vip2': 'Info: Bekommt 40% auf alle Fahrzeuge'
+            'rudi makarov vip2': 'Info: Bekommt 40% auf alle Fahrzeuge'
             
 
             // Hier kannst du weitere Zuordnungen und Infos hinzufügen
