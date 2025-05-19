1719145a2afd123465b8c0472296d54445b857d9
garana123 <jasi.lindern@gmail.com>
2025-05-19 19:40:20 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -285,7 +285,6 @@ document.addEventListener('DOMContentLoaded', () => {
             'black hand': 'Primär: Jagtgrün <br> Sekundär: - <br> Perleffekt: Bonbon Rot',
             'redline': 'Primär: Dunkelgrau <br> Sekundär: - <br> Perleffekt: Zuckerpflaume',
             'van de straat': 'Primär: Schwarz Matt <br> Sekundär: Schwarz Matt <br> Perleffekt: Lachsrosa',
-            'fib dn 60 vip2': 'Info: Bekommt 40% auf alle Fahrzeuge',
             'familie makarov vip2': 'Info: Bekommen 40% auf alle Fahrzeuge',
             'rudi makarov vip2': 'Info: Bekommt 40% auf alle Fahrzeuge'
             
