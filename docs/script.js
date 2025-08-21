b35c70468fe42afdabfa88733310d80274648a4b
lxDexterMorganxl <anon-viper@hotmail.de>
2025-08-21 18:04:29 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -279,6 +279,7 @@ document.addEventListener('DOMContentLoaded', () => {
             'grove street ug': 'Primär: Hellblau <br> Sekundär: Hellblau - <br> Perleffekt: Rasengrün',
             'vagos': 'Primär: Renn Gelb <br> Sekundär: Renn Gelb <br> Perleffekt: Alabasta Weiß',
             'aztecas': 'Primär: Paradise <br> Sekundär: - <br> Perleffekt: Alabasta Weiß',
+            'midnight': 'Primär: Schnee Weiß <br> Sekundär: Schnee Weiß <br> Perleffekt: Helees Gold',
             
 
             // Hier kannst du weitere Zuordnungen und Infos hinzufügen
