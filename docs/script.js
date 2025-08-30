b39b0419ec052ef528df83e60a5cffa39a915d0f
RobMorgen <robinborgmann93@gmail.com>
2025-08-31 01:38:50 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -289,8 +289,8 @@ document.addEventListener('DOMContentLoaded', () => {
             'lcn': 'Primär: Dunkles Silber <br> Sekundär: - <br> Perleffekt: Helles Gold',
             'los sombra': 'Primär: Carbanet Rot <br> Sekundär: - <br> Perleffekt: Zuckerpflaume',
             'blue reign': 'Primär: #edfffd (Metall) <br> Sekundär: - <br> Perleffekt: Helles Blau ',
-            'zivi vip eins': 'Zivis mit einer Visumstufe unter 5',
-            'zivi vip zwei': 'Zivis mit einer Visumstufe zwischen 5-10',
+            'zivi groß': 'Zivis mit einer Visumstufe unter 5',
+            'zivi klein': 'Zivis mit einer Visumstufe zwischen 5-10',
             
             
 
